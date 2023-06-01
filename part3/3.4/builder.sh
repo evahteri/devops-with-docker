caefc1a02ff876a582efe876cde7e6ae2e5d33d8
Eero Vahteri <eero.vahteri@helsinki.fi>
2023-06-01 08:57:19 +0300

    3.4 done

---
+++ part3/3.4/builder.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+## USAGE:
+
+# The script takes two arguments: 
+# - Git repository in the form of <username>/<repository>
+# - Dockerhub repository in the form of <username>/<repository>
+
+# Run in Linux environment: bash builder.sh <git-username>/<git-repository> <dockerhub-username>/<dockerhub-repository>
+
+# Notice: You have to be logged in to Dockerhub for this to work. You might need sudo with the command.
+
+git clone https://github.com/$1
+
+git_repository=$(basename "$1")
+
+cd $git_repository
+
+docker build . -t $2
+
+docker login -u $DOCKER_USER -p $DOCKER_PWD
+
+docker push $2
